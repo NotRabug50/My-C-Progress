7e3d9e62853463614a465da008b32116eebf12de
Rabug50 <smallhacker709@gmail.com>
2023-05-19 14:33:54 +0300

    second

--- tutorial-2.c
+++ tutorial-2.c
@@ -1,17 +1,31 @@
 #include<stdio.h>
+#include<unistd.h>
 
 //A size calculator based of a formula
 int main(){
 
-    int your_fathers_size = 180;
-    int your_moms_size = 167;
+    unsigned int your_fathers_size; //Unsigned because it dont need negative number length
+    unsigned int your_moms_size; //Unsigned because it dont need negative number length
 
-    int are_you_a_boy = 13; //For boys 13, For girls -13
+    short are_you_a_boy = 13; //Not unsigned because it needs negative number length
 
-    int total = your_fathers_size+your_moms_size+are_you_a_boy; //Gets total of variables
-    total = total/2; //Gets half of the total
+    printf("Enter your fathers size (only numbers): "); //Writing console to ask question
+    fflush(stdout); //idk what is this
+    scanf("%d\n", &your_fathers_size); //Waits and input and sets is input value to your fathers size
+
+    printf("Enter your mothers size (only numbers): "); //Writing console to ask question
+    fflush(stdout); //idk what is this
+    scanf("%d\n", &your_moms_size); //Waits and input and sets is input value to your mothers size
+
+    printf("Are you a boy? (13 for yes, -13 for no): "); //Writing console to ask question
+    fflush(stdout); //idk what is this
+    scanf("%d\n", &are_you_a_boy); //Waits and input and sets is input value to are you a boy
+
+    int total = your_fathers_size+your_moms_size+are_you_a_boy; //Getting total of your moms and dads size
+    total = total/2; //Getting half of total
 
     printf("Your size is: %dcm\n", total); //Writes to console
+    sleep(3); // Waiting 3 seconds to close
 
     return 0;
 }
