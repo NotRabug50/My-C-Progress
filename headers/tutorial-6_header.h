014bff973f71a04458aec946523a1ec29f0c5f89
Rabug50 <smallhacker709@gmail.com>
2023-05-20 09:21:44 +0300

    update

---
+++ headers/tutorial-6_header.h
@@ -0,0 +1,4 @@
+int is_prime(int number);
+int get_square(int number);
+//Test getting the reference value and set it for real
+int add_five(int *number);
--- tutorial-6_functions.c
+++ tutorial-6_functions.c
@@ -1,45 +1,10 @@
 #include<stdio.h>
-#include<math.h>
+#include "headers/tutorial-6_header.h"
 
-int is_prime(int number){
-    //Checking is number lower than 2
-    if (number < 2){
-        return 0;
-    }
-    //Checking number is 2
-    else if (number == 2){
-        return 1;
-    }
-    //Calculating
-    else{
-        for(int i=2; i<number; i++){
-            //Checking is number can be divided by i
-            if(number%i == 0){
-                return 0;
-                break;
-            }
-            //Checking number is greater than i
-            if (i>=number-1){
-                return 1;
-                break;
-            }
-        }
-    }
-}
-
-int get_square(int number){
-    number *= number;
-    return number;
-}
-
-//Test getting the reference value and set it for real
-int add_five(int *number){
-    (*number) += 5; //Adding 5 to reference
-    return *number; //Writing to the ram
-}
+int haha = 16; //Creating haha variable
 
 int main(){
-    int haha = 16;
-    add_five(&haha);
-    printf("%d",is_prime(get_square(haha)));
+    add_five(&haha); //Adding five with functions
+    printf("%d \n",haha); //Testing
+    getchar();
 }
